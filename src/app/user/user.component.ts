dbacafb5b9e72ebc8b3603663318a19e0a1d8b3e
osandrey <osandreyman@gmail.com>
2024-11-14 18:26:44 +0000

    ay 1

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -26,8 +26,6 @@ export class UserComponent {
   @Input({required: true}) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
-
-
   get imagePath(){
     return 'assets/users/' + this.user.avatar;
   }
@@ -37,32 +35,3 @@ export class UserComponent {
     this.select.emit(this.user.id);
   }
 }
-
-
-
-
-
-// export class UserComponent {
-//   selectedUser = DUMMY_USERS[randomIndex];
-//
-//   get imagePath(){
-//     return 'assets/users/' + this.selectedUser.avatar
-//   }
-//
-//   onSelectUser() {
-//     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-//     this.selectedUser =  DUMMY_USERS[randomIndex];
-//     console.log();
-//   }
-// }
-
-// export class UserComponent {
-//   selectedUser = signal(DUMMY_USERS[randomIndex]);
-//
-//   imagePath = computed(()=> 'assets/users/' + this.selectedUser().avatar)
-//
-//   onSelectUser() {
-//     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-//     this.selectedUser.set(DUMMY_USERS[randomIndex]);
-//   }
-// }
